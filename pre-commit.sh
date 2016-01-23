80258a19fadab8d3334b034b9632b08d050efb1b
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-01-23 12:48:59 +0100

    add documentation

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 #
-# Git commit hook to ensure latex document has been compiled.
+# Git commit hook to ensure latex documents are up-to-date. Checks documents on
+# all levels of the repository, given that both '<document>.pdf' and
+# '<document>.tex' exist.
+#
+# Author: salsolatragus
+#
+# Setup: Either copy this script to '<repo>/.git/hooks/pre-commit' or link it
+# there. Make sure it's executable.
+#
+# Credits:
+# - Make latexmk check if document is up-to-date: http://tex.stackexchange.com/a/65257
+# - Make latexmk work with documents in subdirectories: http://tex.stackexchange.com/a/11726
 #
 
-find . -name '*.pdf' | sed 's/\(.*\)\..*/\1/' | while read pdf; do
-  if [ -f $pdf.tex ]; then
-    printf "Checking LaTeX document '$pdf'... "
-    latexmk -e '$pdflatex = $latex = '"'"'internal die_latex %S'"'"'; sub die_latex { die "$_[0] is out of date" }' -pdf -jobname=$pdf $pdf >/dev/null 2>/dev/null 
+find . -name '*.pdf' | sed 's/\(.*\)\..*/\1/' | while read doc; do
+  if [ -f $doc.tex ]; then
+    printf "Checking LaTeX document '$doc.pdf'... "
+    latexmk -e '$pdflatex = $latex = '"'"'internal die_latex %S'"'"'; sub die_latex { die "$_[0] outdated" }' -pdf -jobname=$doc $doc >/dev/null 2>/dev/null
     rc=$?
     if [ $rc != 0 ]; then
-      >&2 echo outdated. Please rebuild before commit!
+      >&2 echo outdated. Please rebuild before commit or skip check with -n!
       exit $rc
     else
       echo up-to-date.
