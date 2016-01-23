94a2db0cb926273efc3147754e8c8fc5c9cdd54f
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-01-23 12:21:06 +0100

    support documents in subdirectories

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,10 +3,10 @@
 # Git commit hook to ensure latex document has been compiled.
 #
 
-ls *.pdf | sed 's/\(.*\)\..*/\1/' | while read pdf; do
+find . -name '*.pdf' | sed 's/\(.*\)\..*/\1/' | while read pdf; do
   if [ -f $pdf.tex ]; then
     printf "Checking LaTeX document '$pdf'... "
-    latexmk -e '$pdflatex = $latex = '"'"'internal die_latex %S'"'"'; sub die_latex { die "$_[0] is out of date" }' -pdf $pdf >/dev/null 2>/dev/null 
+    latexmk -e '$pdflatex = $latex = '"'"'internal die_latex %S'"'"'; sub die_latex { die "$_[0] is out of date" }' -pdf -jobname=$pdf $pdf >/dev/null 2>/dev/null 
     rc=$?
     if [ $rc != 0 ]; then
       >&2 echo outdated. Please rebuild before commit!
